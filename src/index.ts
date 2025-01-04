97ec983db47fa9a426d3d7b2c463b6ae36fbb248
pacexy <pacexy@gmail.com>
2025-01-04 09:17:37 +0800

    feat: add openauth

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { authorizer } from '@openauthjs/openauth';
+import { CloudflareStorage } from '@openauthjs/openauth/storage/cloudflare';
+import { type ExecutionContext, type KVNamespace } from '@cloudflare/workers-types';
+import { subjects } from './subjects';
+import { PasswordAdapter } from '@openauthjs/openauth/adapter/password';
+import { PasswordUI } from '@openauthjs/openauth/ui/password';
+
+async function getUser(email: string) {
+	// Get user from database
+	// Return user ID
+	return '123';
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		return authorizer({
+			storage: CloudflareStorage({
+				namespace: env.kv,
+			}),
+			subjects,
+			providers: {
+				password: PasswordAdapter(
+					PasswordUI({
+						sendCode: async (email, code) => {
+							console.log(email, code);
+						},
+					})
+				),
+			},
+			success: async (ctx, value) => {
+				if (value.provider === 'password') {
+					return ctx.subject('user', {
+						id: await getUser(value.email),
+					});
+				}
+				throw new Error('Invalid provider');
+			},
+		}).fetch(request, env, ctx);
 	},
-} satisfies ExportedHandler<Env>;
+};
